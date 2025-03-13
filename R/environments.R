e878abc994ad9b04ced2b41bb6c12066df10a441
leopoldguyot <leopold.guyot13@gmail.com>
2025-03-13 21:41:27 +0100

    docs

--- R/environments.R
+++ R/environments.R
@@ -1,3 +1,75 @@
+#' Environment Management Functions
+#'
+#' These functions allow users to create, clear, and list environments stored in the ".envs" directory.
+#'
+#' @name environment_management
+#' @rdname environment_management
+#' @param envName A `character(1)` string specifying the name of the environment.
+#' @param packages A `character()` vector of package names to install in the environment. Default is NULL.
+#' @param lockfile A `character()` string specifying the path to a lockfile. Default is NULL.
+#' @details Description of functions:
+#' \itemize{
+#'      \item `createEnvironment()`: Creates an environment directory and
+#'       installs the specified packages or uses the lockfile.
+#'      \item `clearEnvironment()`: Deletes a specific environment directory.
+#'      \item `clearEnvironments()`: Deletes all environment directories.
+#'      \item `listEnvironments()`: Returns a character vector containing
+#'       the names of all existing environments.
+#' }
+#' @examples
+#' createEnvironment("my_first_env", packages = c("digest@0.6.18"))
+#' createEnvironment("my_second_env", packages = c("digest@0.6.17"))
+#' listEnvironments()
+#' clearEnvironment("my_first_env")
+#' listEnvironments()
+#' clearEnvironments()
+#'
+#' @export
+createEnvironment <- function(envName, packages = NULL, lockfile = NULL) {
+    envPath <- file.path(".envs", envName)
+    if (!dir.exists(envPath)) {
+        dir.create(envPath, recursive = TRUE)
+    }
+    if (!is.null(packages) && !is.null(lockfile)) {
+        stop("Specify either 'packages' or 'lockfile', not both.")
+    } else if (!is.null(packages)) {
+        .createEnvFromPackagesList(envPath, packages)
+    } else if (!is.null(lockfile)) {
+        .createEnvFromLockFile(envPath, lockfile)
+    } else {
+        stop("Either 'packages' or 'lockfile' must be provided.")
+    }
+}
+
+#' @rdname environment_management
+#' @export
+clearEnvironment <- function(envName) {
+    envPath <- file.path(".envs", envName)
+    if (!dir.exists(envPath)) {
+        stop("Environment directory does not exist: ", envPath)
+    }
+    unlink(envPath, recursive = TRUE, force = TRUE)
+    message("Cleared environment: ", envName)
+}
+
+#' @rdname environment_management
+#' @export
+clearEnvironments <- function() {
+    unlink(".envs", recursive = TRUE, force = TRUE)
+    message("All environments cleared")
+}
+
+#' @rdname environment_management
+#' @return `listEnvironments()`: A `character()` vector that contains
+#'  the environments' names
+#' @export
+listEnvironments <- function() {
+    envs <- list.dirs(".envs", recursive = FALSE, full.names = FALSE)
+    return(envs)
+}
+
+#' @importFrom callr r
+#' @importFrom renv init load install snapshot
 .createEnvFromPackagesList <- function(envPath, packages) {
     envPath <- normalizePath(envPath, mustWork = FALSE)
     callr::r(function(envPath, packages) {
@@ -15,10 +87,12 @@
     }, args = list(envPath, packages))
 }
 
+#' @importFrom callr r
+#' @importFrom renv init restore
 .createEnvFromLockFile <- function(envPath, lockfile) {
     if (!requireNamespace("renv", quietly = TRUE)) {
-    stop("The 'renv' package is required. Install it using install.packages('renv').")
-  }
+        stop("The 'renv' package is required. Install it using install.packages('renv').")
+    }
     envPath <- normalizePath(envPath, mustWork = FALSE)
     callr::r(function(envPath, lockfile) {
         if (!requireNamespace("renv", quietly = TRUE)) {
@@ -32,51 +106,4 @@
         renv::restore(project = envPath, prompt = FALSE)
         message("Environment loaded from lockfile in: ", envPath)
     }, args = list(envPath, lockfile))
-}
-
-createEnvironment <- function(envName, packages = NULL, lockfile = NULL) {
-    envPath <- file.path(".envs", envName)
-    if (!dir.exists(envPath)) {
-        dir.create(envPath, recursive = TRUE)
-    }
-    if (!is.null(packages) && !is.null(lockfile)) {
-        stop("Specify either 'packages' or 'lockfile', not both.")
-    } else if (!is.null(packages)) {
-        .createEnvFromPackagesList(envPath, packages)
-    } else if (!is.null(lockfile)) {
-        .createEnvFromLockFile(envPath, lockfile)
-    } else {
-        stop("Either 'packages' or 'lockfile' must be provided.")
-    }
-}
-
-clearEnvironment <- function(envName) {
-    envPath <- paste0(".envs/", envName)
-    if (!dir.exists(envPath)) {
-        stop("Environment directory does not exist: ", envPath)
-    }
-    unlink(envPath, recursive = TRUE, force = TRUE)
-    message("Cleared environment: ", envPath)
-}
-
-clearEnvironments <- function() {
-    dir <- ".envs"
-    if (!dir.exists(dir)) {
-        stop("Directory does not exist: ", dir)
-    }
-    envs <- list.dirs(dir, recursive = FALSE, full.names = FALSE)
-    if (length(envs) == 0){
-        warning("No environments to clear")
-    } else {
-        for (env in envs) {
-            clearEnvironment(env)
-        }
-        message("All environments cleared")
-    }
-
-}
-
-listEnvironments <- function() {
-    envs <- list.dirs(".envs", recursive = FALSE, full.names = FALSE)
-    return(envs)
 }
--- R/run.R
+++ R/run.R
@@ -1,4 +1,31 @@
-runInEnvironment <- function(env, expr) {
+#' Run expressions in environments
+#'
+#' These functions allow execution of R expressions
+#'  within specified environments.
+#'\itemize{
+#'   \item `runInEnvironment(expr, env)`: Runs an expression in a
+#'    specific environment.
+#'   \item `runInEnvironments(expr, envs)`: Runs an expression across
+#'    multiple environments.
+#' }
+#' @param expr An R expression to be evaluated.
+#' @param env A `character(1)` string specifying the environment name.
+#'
+#' @return
+#' \itemize{
+#'   \item The result of the evaluated expression for `runInEnvironment`.
+#'   \item A list of results for each environment for `runInEnvironments`.
+#' }
+#' @examples
+#' createEnvironment("my_env", packages = c("digest@0.6.18"))
+#' runInEnvironment(print("Hello from env"), "my_env")
+#' runInEnvironments(print("Running in multiple envs"))
+#'
+#' @importFrom callr r
+#' @importFrom renv load
+#' @rdname run_in_environment
+#' @export
+runInEnvironment <- function(expr, env) {
     envPath <- file.path(".envs", env)
     if (!dir.exists(envPath)) {
         stop("Environment does not exist: ", env)
@@ -7,13 +34,9 @@ runInEnvironment <- function(env, expr) {
     result <- callr::r(
         function(envPath, expr) {
             library(renv)
-
             renv::load(project = envPath)
-
-            # add local path to .libPaths()
             libPath <- c(.libPaths(), renv::paths$library(project = envPath))
             .libPaths(libPath)
-
             eval(expr, envir = .GlobalEnv)
         },
         args = list(envPath = envPath, expr = expr)
@@ -21,6 +44,13 @@ runInEnvironment <- function(env, expr) {
     return(result)
 }
 
+#' @param envs A `character()` vector of environment names.
+#'  Default is `listEnvironments()`.
+#'
+#' @importFrom callr r
+#' @importFrom renv load
+#' @rdname run_in_environment
+#' @export
 runInEnvironments <- function(expr, envs = listEnvironments()) {
     results <- list()
     for (env in envs) {
